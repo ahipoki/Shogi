b7af1bc97c5084b50ef8f275dc8fdf11af3efca2
ahipoki <50777654+ahipoki@users.noreply.github.com>
2019-09-26 21:16:37 -0700

    Update Shogi.cpp

--- Shogi.cpp
+++ Shogi.cpp
@@ -8,6 +8,17 @@ using namespace std;
  9/26/19
  A shogi program
 */
+void showBoard(char board[] [SIDE]) {
+ printf("\n\n");
+ 
+ printf("\t\t\t  %c | %c  | %c  \n", board[0][0], board[0][1], board[0][2]);
+ printf("\t\t\t--------------\n");
+ printf("\t\t\t  %c | %c  | %c  \n"", board[1][0], board[1][1], board[1][2]);
+ printf("\t\t\t--------------\n");
+ printf("\t\t\t  %c | %c  | %c  \n\n", board[2][0], board[2][1], board[2][2]);
+ 
+ return;
+}
 
 int main() {
  //9x9 board
