07f7e4c0d03fb6aceeeea351813abde0526a7f81
ahipoki <50777654+ahipoki@users.noreply.github.com>
2019-10-04 08:45:39 -0700

    Update Shogi.cpp

--- Shogi.cpp
+++ Shogi.cpp
@@ -166,8 +166,24 @@ void Board::setBoard()
  
  for (int i = 2; i < 6; i++)
  {
-  
+  for (int j = 0; j < 8; j++)
+  {
+   square[i][j].setX(i);
+   square[i][j].setY(j);
+  }
  }
+}
+
+bool Board::playGame()
+{
+ system("cls");
+ printBoard();
+ return doMove();
+}
+
+bool Board::moveKing(Square* thisKing, Square* thatSpace)
+{
+ 
 }
 void shogi() {
  int player1Wins = 0;
