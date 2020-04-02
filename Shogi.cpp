9d7d6ef6b6bb5757cd979c1cda68d844b7f91412
ahipoki <50777654+ahipoki@users.noreply.github.com>
2020-04-02 12:22:10 -0700

    Update Shogi.cpp

--- Shogi.cpp
+++ Shogi.cpp
@@ -1,24 +1,42 @@
 #include <iostream>
 #include <string>
-#include "Chess.h"
+#include "Shogi.h"
+
+using namespace std;
+
+void strupper(char* str);
 
 int main()
 {
     Board b;
-    std::string s;
+    char input[80];
+    char move[1];
+    bool gameOver = false;
     bool newGame = true;
-    std::cout<<"   _____ _    _ ______  _____ _____ \n  / ____| |  | |  ____|/ ____/ ____| \n | |    | |__| | |__  | (___| (___  \n | |    |  __  |  __|  \\___  \\___ \\ \n | |____| |  | | |____ ____) |___) | \n  \\_____|_|  |_|______|_____/_____/ \n"<<std::endl;
-    std::cout<<"Enter any key to continue"<<std::endl;
-    std::cin>>s;
-    
+    cout<<" ________            _______  _______ _________ \n(  _____ \\|\\      /|(  ___  )(  ____ \\\\__   __/ \n| (     \\/| )    ( || (   ) || (    \\/   ) ( \n| (______ | (____) || |   | || |         | | \n(______  )|  ____  || |   | || | ____    | | \n       ) || (    ) || |   | || | \\_  )   | | \n/\\_____) || )    ( || (___) || (___) |___) (___ \n\\________)|/      \\|(_______)(_______)\\_______/"<<endl;
+    cout<<"Enter any key to continue"<<std::endl;
+    cin.getline(move, 1);
     while (newGame){
         b.setBoard();
         while (b.playGame());
-        std::cout<<"Do you want to play again?"<<std::endl;
-        std::cin>>s;
-        if (s != "yes"){
+        //if (gameOver == true){
+        cout<<"Do you want to play again?"<<endl;
+        cin.getline(input, sizeof(input));
+        strupper(input);
+        if (strcmp(input, "YES") == 0){
+            newGame = true;
+        }
+        else if (strcmp(input, "NO") == 0){
             newGame = false;
         }
     }
+    //}
     return 0;
+}
+
+void strupper(char* str){
+    int len = strlen(str);
+    for (int i = 0; i < len; i++){
+        str[i] = toupper(str[i]);
+    }
 }
